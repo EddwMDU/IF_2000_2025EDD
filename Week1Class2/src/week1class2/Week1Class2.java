ee121b77669f76951cca2f1e254e8fa00d28af66
Edwim Donaire <edwim.sts@gmail.com>
2025-08-14 11:25:45 -0600

    Mi Primer Commit en Java

---
+++ Week1Class2/src/week1class2/Week1Class2.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week1class2;
+
+/**
+ *i
+ * @author edwim
+*/ 
+
+/* Library*/
+
+
+public class Week1Class2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     //Data types
+     
+     /*String name = "Eddw";
+     
+    /* int age = 22;
+     
+    /* 
+     Tipos de datos numericos:
+     int, long, short, float, double,
+     numeric, money, decimal
+     
+     Manejo de fechas
+     Date
+     Cadena de caracteres:
+     String
+     
+     Objectos:
+     Object
+     
+     Estructuras de datos:
+     Arrays o Vectors: int [] x 
+                           int [][] y;
+     True/False
+     booleans
+     */
+    /* 
+    System.out.println("Hello Word");
+    System.out.println("Name: "+name);
+    System.out.println("Age: "+age);
+    
+    /* Python:
+    print ('Hello Word')
+    */
+    }
+    
+     
+}
